07354618170b3e6f1ff87ba9854ed1cfc19dd041
Max Ott <max.ott@data61.csiro.au>
2024-05-06 10:35:30 +1000

    build maintenance

--- src/ivcap_sdk_service/aspect.py
+++ src/ivcap_sdk_service/aspect.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2023 Commonwealth Scientific and Industrial Research Organisation (CSIRO). All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file. See the AUTHORS file for names of contributors.
+#
 import json
 from typing import Dict
 from pydantic import RootModel, TypeAdapter
