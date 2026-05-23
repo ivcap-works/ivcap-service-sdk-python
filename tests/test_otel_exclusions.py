ba5cfa78a57da151ff567de0a7c18ddbc2d9742f
Max Ott <max.ott@data61.csiro.au>
2026-05-23 15:54:47 +1000

    better tests

--- tests/test_otel_exclusions.py
+++ tests/test_otel_exclusions.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2026 Commonwealth Scientific and Industrial Research Organisation (CSIRO). All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file. See the AUTHORS file for names of contributors.
+#
 """Tests for OTEL URL exclusion functionality.
 
 This module tests the `_build_excluded_urls()` function that prevents
